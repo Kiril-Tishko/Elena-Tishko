6ca1a85176f78a92fbaa834b4976b962f58566fb
Kiril-Tishko <Kiril-Tishko@users.noreply.github.com>
2019-08-23 20:23:58 +0300

    img size

--- js/main.js
+++ js/main.js
@@ -37,15 +37,16 @@ window.addEventListener('click', function (e) {
 
 
 // Media
-const viber = document.getElementById('viber')
-const phone = document.getElementById('phone')
+// const viber = document.getElementById('viber')
+// const phone = document.getElementById('phone')
 
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-	viber.removeAttribute('href')
-	viber.setAttribute('href', 'viber://add?number=380969409698')
-	phone.setAttribute('href', 'tel:+380969409698')
-	phone.setAttribute('title', 'Позвонить сейчас')
-}
+// if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+// 	viber.removeAttribute('href')
+// 	viber.setAttribute('href', 'viber://add?number=380969409698')
+// 	console.log(viber.getAttribute('href'))
+// 	phone.setAttribute('href', 'tel:+380969409698')
+// 	phone.setAttribute('title', 'Позвонить сейчас')
+// }
 
 // const gallery = document.getElementById('gallery')
 // const windowMaxWidth = window.matchMedia('(max-width: 860px)')
